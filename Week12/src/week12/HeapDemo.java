2d2fbe2b861f36088073fb4ae11fde7cdebf905c
kingstrike <quixwnsxo@naver.com>
2022-06-04 21:22:08 +0900

    12주차

---
+++ Week12/src/week12/HeapDemo.java
@@ -0,0 +1,10 @@
+package week12;
+
+public class HeapDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Week12/src/week12/PQExceotion.java
@@ -0,0 +1,7 @@
+package week12;
+
+public class PQException extends Exception {
+	public PQException(String msg) {
+		super(msg);
+	}
+} 
